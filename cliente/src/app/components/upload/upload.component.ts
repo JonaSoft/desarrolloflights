7d880140ea5a731d5fad919ae1780ff0f7f91e8b
Jonasoft <carlosucananbarrera@hotmail.com>
2020-01-05 19:23:19 -0500

    1er prototipo desarrollado

---
+++ cliente/src/app/components/upload/upload.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+export class UploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
